d58de1c22904ee314af66b827ce0993a2ee76e7a
hanningning0507 <hnn_1125838029@163.com>
2021-09-28 16:21:58 +0800

    dhsahdj

---
+++ demo2.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: your name
+ * @Date: 2021-09-28 16:20:51
+ * @LastEditTime: 2021-09-28 16:20:52
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \a\learngit\demo2.js
+ */
+
+let sum = 0;
